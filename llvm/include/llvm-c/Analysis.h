63ccefd993e240bde8bee98857a7501d41a93082
thanhtoantnt <thanhtoantnt@gmail.com>
2022-06-23 14:41:20 +0800

    start binding of alias analysis

--- llvm/include/llvm-c/Analysis.h
+++ llvm/include/llvm-c/Analysis.h
@@ -38,6 +38,21 @@ typedef enum {
 } LLVMVerifierFailureAction;
 
 
+typedef enum {
+  /// The two locations do not alias at all.
+  LLVMNoAlias,
+
+  /// The two locations may or may not alias. This is the least precise
+  /// result.
+  LLVMMayAlias,
+
+  /// The two locations precisely alias each other.
+  LLVMMustAlias,
+
+  /// The two locations alias, but only due to a partial overlap.
+  LLVMPartialAlias,
+} LLVMAliasResult;
+
 /* Verifies that a module is valid, taking the specified action if not.
    Optionally returns a human-readable description of any invalid constructs.
    OutMessage must be disposed with LLVMDisposeMessage. */
@@ -52,6 +67,8 @@ LLVMBool LLVMVerifyFunction(LLVMValueRef Fn, LLVMVerifierFailureAction Action);
    Useful for debugging. */
 void LLVMViewFunctionCFG(LLVMValueRef Fn);
 void LLVMViewFunctionCFGOnly(LLVMValueRef Fn);
+
+LLVMAliasResult LLVMAlias(LLVMValueRef V1, LLVMValueRef V2);
 
 /**
  * @}
